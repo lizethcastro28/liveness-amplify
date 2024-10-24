89bc54425d2dc30f74cdc117f077d73e16236f06
Lizeth Castro <lcastro@MacBook-Air-de-Lizeth.local>
2024-10-24 15:30:38 -0400

    sin proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://biometric.integrationlayer.com', // Asegúrate de usar HTTPS
-        changeOrigin: true,
-        secure: false, // Desactiva la verificación SSL si es necesario
-        rewrite: (path) => path.replace(/^\/api/, ''), // Reescribir la ruta
-      },
-    },
-  },
-});
+})
+
